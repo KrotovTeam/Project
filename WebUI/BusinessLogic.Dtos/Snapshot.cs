781d86d7b98aff315c445dff2eb5fea5b6bb60dd
Mzhach <mzhaches@mail.ru>
2017-05-29 22:53:45 +0300

    Добавлен интерфейс DownloadManager + его реализация заглушкой.

---
+++ WebUI/BusinessLogic.Dtos/Snapshot.cs
@@ -0,0 +1,20 @@
+﻿using Common.Constants;
+
+namespace BusinessLogic.Dtos
+{
+    /// <summary>
+    /// Cнимок
+    /// </summary>
+    public class Snapshot
+    {
+        /// <summary>
+        /// Путь к файлу
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Канал
+        /// </summary>
+        public ChannelEnum Channel { get; set; }
+    }
+}
---
+++ WebUI/BusinessLogic.Managers/DownloadManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogic.Abstraction;
+using BusinessLogic.Dtos;
+using Common.Constants;
+
+namespace BusinessLogic.Managers
+{
+    /// <summary>
+    /// Менеджер для скачивания снимков
+    /// </summary>
+    public class DownloadManager : IDownloadManager
+    {
+        /// <summary>
+        /// Скачивание снимков
+        /// </summary>
+        /// <param name="point1">Точка 1</param>
+        /// <param name="point2">Точка 2</param>
+        /// <param name="point3">Точка 3</param>
+        /// <param name="point4">Точка 4</param>
+        /// <param name="date">Дата</param>
+        /// <param name="channels">Необходимые каналы</param>
+        /// <returns>Пути к сохраненным снимкам</returns>
+        public IList<Snapshot> DownloadSnapshots(GeographicalPoint point1, GeographicalPoint point2, GeographicalPoint point3,
+            GeographicalPoint point4, DateTime date, IList<ChannelEnum> channels)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
