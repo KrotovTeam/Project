862626995578e77f248c1d30f0b4276c6e95e973
Mzhach <mzhaches@mail.ru>
2017-06-04 19:34:29 +0300

    Доработана карта

--- WebUI/WebUI/Controllers/MapController.cs
+++ WebUI/WebUI/Controllers/MapController.cs
@@ -1,10 +1,18 @@
 ﻿using System.Web.Mvc;
+using BusinessLogic.Dtos;
 
 namespace WebUI.Controllers
 {
     public class MapController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Snapshots(GeographicalPoint upperRight)
         {
             return View();
         }
--- WebUI/WebUI/Scripts/Map/map.js
+++ WebUI/WebUI/Scripts/Map/map.js
@@ -5,43 +5,41 @@ var map;
 var rectangle;
 
 var init = function() {
-    ymaps.ready(function () {
-        map = new ymaps.Map("map", {
-            center: [44.547521, 33.705542],
-            zoom: 10
-        });
-        map.events.add('click', function (e) {
-            if (points.length < 2) {
-                var coords = e.get('coords');
-                var placemark = new ymaps.Placemark([coords[0], coords[1]], {
-                    preset: 'islands#icon',
-                    iconColor: '#0095b6'
-                }, {
-                    draggable: true
-                });
+    map = new ymaps.Map("map", {
+        center: [44.547521, 33.705542],
+        zoom: 10
+    });
+    map.events.add('click', function (e) {
+        if (points.length < 2) {
+            var coords = e.get('coords');
+            var placemark = new ymaps.Placemark([coords[0], coords[1]], {
+                preset: 'islands#icon',
+                iconColor: '#0095b6'
+            }, {
+                draggable: true
+            });
 
-                placemark.events.add('click', function () {
-                    map.geoObjects.remove(placemark);
-                    var index = points.findIndex(obj => obj.geometry._coordinates[0] === placemark.geometry._coordinates[0] &&
-                        obj.geometry._coordinates[1] === placemark.geometry._coordinates[1]);
-                    points.splice(index, 1);
-                    if (points.length === 1) {
-                        $("#map").trigger("removeRectangle");
-                    }
-                });
-
-                placemark.events.add("dragend", function () {
-                    map.geoObjects.remove(rectangle);
-                    $("#map").trigger("addRectangle");
-                });
-
-                map.geoObjects.add(placemark);
-                points.push(placemark);
-                if (points.length === 2) {
-                    $("#map").trigger("addRectangle");
+            placemark.events.add('click', function () {
+                map.geoObjects.remove(placemark);
+                var index = points.findIndex(obj => obj.geometry._coordinates[0] === placemark.geometry._coordinates[0] &&
+                    obj.geometry._coordinates[1] === placemark.geometry._coordinates[1]);
+                points.splice(index, 1);
+                if (points.length === 1) {
+                    $("#map").trigger("removeRectangle");
                 }
+            });
+
+            placemark.events.add("dragend", function () {
+                map.geoObjects.remove(rectangle);
+                $("#map").trigger("addRectangle");
+            });
+
+            map.geoObjects.add(placemark);
+            points.push(placemark);
+            if (points.length === 2) {
+                $("#map").trigger("addRectangle");
             }
-        });
+        }
     });
 
     $("#map").on("addRectangle", function () {
